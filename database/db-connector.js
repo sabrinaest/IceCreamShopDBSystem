04735cd20daac739f83109f5b3cfa99f7cecde37
Sabrina Estrada <102570901+sabrinaest@users.noreply.github.com>
2023-09-26 23:52:16 -0700

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     host            : 'localhost',
     user            : '',
     password        : '',
-    database        : ''
+    database        : 'sunshinescoops'
 });
 
 
